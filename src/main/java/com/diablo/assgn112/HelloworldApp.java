706b57af0d49e8b425d9ae0ba54be932120ddb04
diablo <diablo@diablo-Latitude-E5520>
2017-08-01 11:17:18 +0530

    assgn 11.2

---
+++ src/main/java/com/diablo/assgn112/HelloworldApp.java
@@ -0,0 +1,11 @@
+package com.diablo.assgn112;
+
+public class HelloworldApp {
+ private String message;
+ public void setMessage(String message){
+     this.message  = message;
+  }
+  public void getMessage(){
+     System.out.println(": " + message);
+  }
+}
